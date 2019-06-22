41ffae9156be60a6cf9b98e434516ee146bfebac
Mai6666 <primoliebe@yahoo.co.jp>
2019-06-22 23:27:59 +0900

    mod length

--- test/Lux/integration.cpp
+++ test/Lux/integration.cpp
@@ -20,14 +20,14 @@ int main(){
 	int stat = I2C_Open(0,0x39);
 	if(stat != 0) std::cout << "Open error" << std::endl;
 	
-	std::cout << I2C_WriteByte(0, 0x01A0, 4) << std::endl;
-	std::cout << I2C_WriteByte(0, 0x00AD, 4) << std::endl;
-	std::cout << I2C_WriteByte(0, 0x01AF, 4) << std::endl;
-	std::cout << I2C_WriteByte(0, 0xB6A1, 4) << std::endl;
-	std::cout << I2C_WriteByte(0, 0x03A0, 4) << std::endl;
+	std::cout << I2C_WriteByte(0, 0x01A0, 2) << std::endl;
+	std::cout << I2C_WriteByte(0, 0x00AD, 2) << std::endl;
+	std::cout << I2C_WriteByte(0, 0x01AF, 2) << std::endl;
+	std::cout << I2C_WriteByte(0, 0xB6A1, 2) << std::endl;
+	std::cout << I2C_WriteByte(0, 0x03A0, 2) << std::endl;
 
 	for(int i=0; i<1; i++){
-		I2C_WriteByte(0, 0x14+0xA0, 4);
+		I2C_WriteByte(0, 0x14+0xA0, 2);
 		lux = I2C_ReadWord(0);
 		//std::cout << std::bitset<32>(lux);//
 		std::cout << std::hex << lux << std::endl;
