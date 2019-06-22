b368758d6f278a1a079a2b3adefb4ed87db6360c
mai <m5221116@u-aizu.ac.jp>
2019-06-22 23:04:25 +0900

    add test

---
+++ test/Lux/integration.cpp
@@ -0,0 +1,38 @@
+extern void I2C_Init( void );
+extern void I2C_Close( int ch );
+extern void I2C_Term( void );
+extern int I2C_Open( int ch, int adr );
+extern int I2C_ReadByte( int ch );
+extern int I2C_ReadWord( int ch );
+extern int I2C_WriteByte( int ch, int value, int length );
+
+#include <iostream>
+#include <unistd.h>
+#include <bitset>
+
+using std::cout;
+using std::endl;
+
+int main(){
+	int lux=5;
+
+	I2C_Init();
+	int stat = I2C_Open(0,0x39);
+	if(stat != 0) std::cout << "Open error" << std::endl;
+	
+	std::cout << I2C_WriteByte(0, 0x0180, 2) << std::endl;
+	std::cout << I2C_WriteByte(0, 0x0F+0x00, 2) << std::endl;
+	std::cout << I2C_WriteByte(0, 0x0D+0x04, 2) << std::endl;
+	std::cout << I2C_WriteByte(0, 0x0380, 2) << std::endl;
+
+	for(int i=0; i<1; i++){
+		I2C_WriteByte(0, 0x14+0x80, 4);
+		lux = I2C_ReadWord(0);
+		//std::cout << std::bitset<32>(lux);//
+		std::cout << std::hex << lux << std::endl;
+		usleep(50000);
+	}
+
+	I2C_Close(0);
+	return 0;
+}
---
+++ test/Lux/lux.cpp
@@ -0,0 +1,33 @@
+extern void I2C_Init( void );
+extern void I2C_Close( int ch );
+extern void I2C_Term( void );
+extern int I2C_Open( int ch, int adr );
+extern int I2C_ReadByte( int ch );
+extern int I2C_ReadWord( int ch );
+extern int I2C_WriteByte( int ch, int value, int length );
+int get_lux(void);
+
+#include <iostream>
+#include <unistd.h>
+#include <bitset>
+
+using std::cout;
+using std::endl;
+
+int main(){
+	int lux=5;
+
+	I2C_Init();
+	int stat = I2C_Open(0,0x39);
+	if(stat != 0) std::cout << "Open error" << std::endl;
+
+	for(int i=0; i<1; i++){
+		lux = get_lux();
+		//std::cout << std::bitset<32>(lux);//
+		std::cout << lux << std::endl;
+		usleep(50000);
+	}
+
+	I2C_Close(0);
+	return 0;
+}
